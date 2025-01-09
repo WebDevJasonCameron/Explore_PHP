f22fc3a00c7593a430b5c5fbe2825b4a678c86ae
Jason Cameron <dev.jasoncameron@gmail.com>
2025-01-08 20:04:22 -0600

    vars

--- Udemy/LearninPHP_Traversy/Student/Section_02/index.php
+++ Udemy/LearninPHP_Traversy/Student/Section_02/index.php
@@ -1,13 +1,8 @@
 <?php
-
-// This is a single line comment
-/*
- * This is multiple line comment
- */
-    echo 'Hello From PHP';
-
+    $title = 'Learn PHP From Scratch';
+    $heading = 'Welcome to the course';
+    $body = 'In this course, you will learn the fundamentals of the PHP language'
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,19 +11,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title><?php echo 'Learn PHP---'; ?></title>
+    <title><?= $title; ?></title>
 </head>
 
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">........</h1>
+            <h1 class="text-3xl font-semibold"><?= $title ?></h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4">Welcome To The Course</h2>
-            <?php echo '<p>In this course, you will learn the fundamentals of the PHP language</p>'; ?>
+            <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></h2>
+            <p><?= $body ?></p>
         </div>
     </div>
 </body>
