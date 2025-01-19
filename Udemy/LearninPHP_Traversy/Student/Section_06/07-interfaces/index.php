c973c54ead2ef76f495e57d0b0c6c2a1687ea420
Jason Cameron <dev.jasoncameron@gmail.com>
2025-01-19 08:21:36 -0600

    section 07 started

--- Udemy/LearninPHP_Traversy/Student/Section_06/07-interfaces/index.php
+++ Udemy/LearninPHP_Traversy/Student/Section_06/07-interfaces/index.php
@@ -1,3 +1,60 @@
+<?php
+    interface ContentInterface {
+        public function display();
+        public function edit();
+    }
+
+    class Article implements ContentInterface {
+
+        // PROPs
+        private string $title;
+        private string $content;
+
+        // CONs
+        public function __construct(string $title, string $content) {
+            $this->title = $title;
+            $this->content = $content;
+        }
+
+        // METHs
+        public function display(): void {
+            echo "<h2>{$this->title}</h2>";
+            echo "<p>{$this->content}</p]>";
+        }
+
+        public function edit(): void {
+            echo "Editing the article '{$this->title}'...";
+        }
+    }
+
+    class Video implements ContentInterface {
+
+        // PROPs
+        private string $title;
+        private string $url;
+
+        // CONs
+        public function __construct(string $title, string $url) {
+            $this->title = $title;
+            $this->url = $url;
+        }
+
+        // METHs
+        public function display(): void {
+            echo "<h2>{$this->title}</h2>";
+            echo "<iframe src='{$this->url}'></iframe>";
+        }
+
+        public function edit(): void {
+            echo "Editing the video '{$this->title}'...";
+        }
+    }
+
+    $article = new Article("Introduction to PHP", "PHP is a versatile scripting language");
+    $video = new Video("PHP for Beginners", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,8 +73,11 @@
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
+        <?= $article->display(); ?>
     </div>
+  </div>
+  <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+      <?= $video->display(); ?>
   </div>
 </body>
 
