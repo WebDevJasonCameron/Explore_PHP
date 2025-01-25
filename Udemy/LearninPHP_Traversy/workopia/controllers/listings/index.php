1f95052793252f91774fbb1dba45821479b31756
Jason Cameron <dev.jasoncameron@gmail.com>
2025-01-25 14:14:47 -0600

    unable to get other pages to link

---
+++ Udemy/LearninPHP_Traversy/workopia/controllers/listings/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// http://localhost:63343/workopia/controllers/listings/index.php?_ijt=3i7l997rdv9rn78laf7s1vhbes&_ij_reload=RELOAD_ON_SAVE
+
+echo 'Listings';
--- Udemy/LearninPHP_Traversy/workopia/public/index.php
+++ Udemy/LearninPHP_Traversy/workopia/public/index.php
@@ -2,27 +2,28 @@
 require '../helpers.php';
 
 $routes = [
-  '/index.php' => 'controllers/home.php',
-  '/listings' => 'controllers/listings/index.php',
+  '/' => 'controllers/home.php',
+  '/listings' => 'workopia/controllers/listings/index.php',
   '/listings/create' => 'controllers/listings/create.php',
   '404' => 'controllers/404.php',
 ];
 
 $basePath = '/workopia/public';
 
+inspect($basePath);
+
 $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
-$uri = str_replace($basePath, '', $uri);
-
-// Test my git
-
-//loadView('home');
 
 inspect($uri);
-//inspectAndDie($routes);
+
+$uri = str_replace($basePath, '', $uri);
+
+$uri = str_replace('index.php', '', $uri);
+
+inspect($uri);
 
 if(array_key_exists($uri, $routes)) {
   require basePath($routes[$uri]);
 } else {
-//  inspect('controllers/404.php');
   require basePath($routes['404']);
 }
