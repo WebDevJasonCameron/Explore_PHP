9179a7e094e8283c41c3faaafddf026e1dd45a93
Jason Cameron <dev.jasoncameron@gmail.com>
2025-02-01 12:14:20 -0600

    using routes with composer

--- Udemy/LearninPHP_Traversy/workopia/App/controllers/HomeController.php
+++ Udemy/LearninPHP_Traversy/workopia/App/controllers/HomeController.php
@@ -1,13 +1,26 @@
 <?php
 
 namespace App\Controllers;
+use Framework\Database;
 
 class HomeController {
+
+  // PROPs
+  protected $db;
+
+  // CONs
   public function __construct() {
-    
+    $config = require basePath('config/db.php');
+    $this->db = new Database($config);
   }
 
+  // METHs
   public function index() {
-    die('HomeController@index');
-  }
+    $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+    loadView("home", 
+            [
+              'listings' => $listings
+            ]
+    );  }
 }
